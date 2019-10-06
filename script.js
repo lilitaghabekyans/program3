29763092e95c2fd770adb5bc0385f330b0885de3
lilitaghabekyans <56076897+lilitaghabekyans@users.noreply.github.com>
2019-10-06 18:01:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+let matrix = [];
+let side = 20;
+let grassArr = [];
+let grassEaterArr = [];
+let gishatichArr = [];
+let boyArr = [];
+let waterArr = [];
+
+function setup() {
+    matrixGenerator(30, 80, 60, 30, 3, 1);
+    frameRate(8);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y);
+                grassArr.push(grass);
+            }
+            if (matrix[y][x] == 2) {
+                let grassEater = new GrassEater(x, y);
+                grassEaterArr.push(grassEater);
+            }
+            if (matrix[y][x] == 3) {
+                let gishatich = new Gishatich(x, y);
+                gishatichArr.push(gishatich);
+            }
+            if (matrix[y][x] == 4) {
+                let boy = new Boy(x, y);
+                boyArr.push(boy);
+            }
+            if (matrix[y][x] == 5) {
+                let water = new Water(x, y);
+                waterArr.push(water);
+            }
+        }
+    }
+    function matrixGenerator(matrixSize, grass, grassEater, gishatich, boy, water) {
+        for (let i = 0; i < matrixSize; i++) {
+            matrix[i] = [];
+            for (let o = 0; o < matrixSize; o++) {
+                matrix[i][o] = 0;
+            }
+        }
+        for (let i = 0; i < grass; i++) {
+            let customX = Math.floor(random(0, matrixSize));
+            let customY = Math.floor(random(0, matrixSize));
+            matrix[customY][customX] = 1;
+        }
+        for (let i = 0; i < grassEater; i++) {
+            let customX = Math.floor(random(0, matrixSize));
+            let customY = Math.floor(random(0, matrixSize));
+            matrix[customY][customX] = 2;
+        }
+        for (let i = 0; i < gishatich; i++) {
+            let customX = Math.floor(random(0, matrixSize));
+            let customY = Math.floor(random(0, matrixSize));
+            matrix[customY][customX] = 3;
+        }
+        for (let i = 0; i < boy; i++) {
+            let customX = Math.floor(random(0, matrixSize));
+            let customY = Math.floor(random(0, matrixSize));
+            matrix[customY][customX] = 4;
+        }
+        for (let i = 0; i < water; i++) {
+            let customX = Math.floor(random(0, matrixSize));
+            let customY = Math.floor(random(0, matrixSize));
+            matrix[customY][customX] = 5;
+        }
+    }
+}
+
+function draw() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                fill("green");
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 2) {
+                fill("orange");
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("black");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("blue");
+            }
+            rect(x * side, y * side, side, side);
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat();
+    }
+    for (var i in gishatichArr) {
+        gishatichArr[i].utel();
+    }
+    for (var i in boyArr) {
+        boyArr[i].eatboy();
+    }
+    for (var i in waterArr) {
+        waterArr[i].mul1();
+    }
+}
